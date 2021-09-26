663122572f1859bc9dad3b8d815c31b8f0045b2e
Thom Bruce <thom@thombruce.com>
2021-09-27 00:47:02 +0100

    Make table sticky in both dimensions

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template lang='pug'>
-article.m-4
-  .overflow-x-auto
-    table.table.w-full
+article
+  .table-container
+    table
       thead
         tr
           th Name
@@ -35,16 +35,46 @@ export default {
 }
 </script>
 
-<style lang='postcss'>
-.table {
-  & th {
-    &:first-child {
-      @apply relative sm:sticky;
-    }
-  }
+<style lang='postcss' scoped>
+.table-container {
+  @apply max-h-screen overflow-auto;
 
-  & td {
-    @apply whitespace-normal;
+  & table {
+    @apply table w-full h-full;
+
+    & th {
+      &:first-child {
+        @apply relative sm:sticky;
+      }
+    }
+
+    & thead {
+      & th {
+        @apply sticky;
+        top: 0;
+        z-index: 20;
+        &:first-child {
+          @apply sticky left-auto sm:left-0;
+          z-index: 30;
+        }
+      }
+    }
+
+    & thead, & tfoot {
+      & th, & td {
+        &:first-child {
+          @apply rounded-l-none;
+        }
+
+        &:last-child {
+          @apply rounded-r-none;
+        }
+      }
+    }
+
+    & td {
+      @apply whitespace-normal;
+    }
   }
 }
 </style>
